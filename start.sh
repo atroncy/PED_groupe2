02dd3c67151542a7f70d08bff47a48a62b51784c
Alexandre Troncy <atroncy@emi.u-bordeaux1.fr>
2015-02-17 16:21:12 +0100

    ouioui

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 	ksu $user login
+#	$1=interface
+
+
+
+sudo /sbin/ardrone-wifi -i $1 -o up $1
+
+sudo /sbin/ardrone-wifi -i $1 -o scan
+
+sudo /sbin/ardrone-wifi -i $1 -o start -s PLOP -m adhoc -c 6 -a 192.168.1.2
+
+sudo /sbin/ardrone-wifi -i $1 -o status
+
+ping -c3 192.168.1.1
